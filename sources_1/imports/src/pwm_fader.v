3522d60c62325ff3c69c6102c5fd7c01ca611fa4
craigo <craig@eldougo.net>
2018-07-24 12:39:49 +1000

    Tidy up code

--- sources_1/imports/src/pwm_fader.v
+++ sources_1/imports/src/pwm_fader.v
@@ -21,26 +21,27 @@
 
 
 module pwm_fader
- #(NUMBER_DEVICES = 16)
-  (
-    input                      clk,
-    input [29:0]               linger_clks,
-    input [3:0]                start_brightness,
-    input [NUMBER_DEVICES-1:0] active,
-    output[NUMBER_DEVICES-1:0] faded_out
-  );
+#(
+  NUMBER_DEVICES = 16
+)(
+  input                      clk,
+  input [29:0]               linger_clks,             // Number of clock cycles between each fade event
+  input [3:0]                start_brightness,        // Brightness of the active device. 0-15
+  input [NUMBER_DEVICES-1:0] active,                  // The active devices at full brightness
+  output[NUMBER_DEVICES-1:0] faded_out                // The output to conect to the devices.
+);
   
-  localparam FADE_SPEED = 4'h1;
-  wire w_fade_pulse;
+  localparam FADE_STEP = 4'h1;
+  wire                      w_fade_pulse;
   wire [NUMBER_DEVICES-1:0] w_faded_out;
   
-  reg [3:0]  dev_pwm[0:NUMBER_DEVICES-1];
-  reg [3:0]  pwm_cntr = 0;
+  reg [3:0]                 dev_pwm[0:NUMBER_DEVICES-1];
+  reg [3:0]                 pwm_cntr = 0;
   
   // Generate fader pulses
   pulse_generator pulse_pwm (.clk(clk), .clks_per_pulse(linger_clks), .pulse(w_fade_pulse));
     
-  // Set the all PWMs to zero 
+  // Initialize all devices to zero brightness. 
   integer i;
   initial
   begin
@@ -56,10 +57,10 @@ module pwm_fader
     for(n = 0; n < NUMBER_DEVICES; n = n + 1)  begin
       if (active[n]) begin
         dev_pwm[n] <= start_brightness;
-      end else if (dev_pwm[n] < FADE_SPEED) begin
+      end else if (dev_pwm[n] < FADE_STEP) begin
         dev_pwm[n] <= 0;
       end else begin
-        dev_pwm[n] <= dev_pwm[n] - FADE_SPEED;
+        dev_pwm[n] <= dev_pwm[n] - FADE_STEP;
       end
     end
   end
